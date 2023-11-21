03f762f83cecdd86472d28d6537acb4db74b6c2a
Radek Bartoň <radek.barton@microsoft.com>
2023-11-21 19:43:39 +0100

    Add Linux build to advanced workflow

--- .github/scripts/build-gcc.sh
+++ .github/scripts/build-gcc.sh
@@ -12,15 +12,39 @@ if [ $RUN_CONFIG = 1 ] || [ ! -f "$GCC_BUILD_PATH/Makefile" ] ; then
 
     rm -rf $GCC_BUILD_PATH/*
 
-    # REMOVED --libexecdir=/opt/lib
-    # REMOVED --with-{gmp,mpfr,mpc,isl}=/usr
-    $SOURCE_PATH/$GCC_VERSION/configure \
+    case $TARGET in
+        *linux*)
+            # REMOVED --with-isl=/usr"
+            HOST_OPTION=" \
+                --enable-shared \
+                --enable-threads=posix \
+                --with-gmp=/usr \
+                --with-mpfr=/usr \
+                --with-mpc=/usr"
+            ;;
+        *) # *mingw*)
+            # REMOVED --enable-shared
+            # ADDED --disable-shared
+            # REMOVED --libexecdir=/opt/lib
+            # REMOVED --with-{gmp,mpfr,mpc,isl}=/usr
+            HOST_OPTION=" \
+                --disable-shared \
+                --enable-threads=win32 \
+                --disable-rpath \
+                --disable-win32-registry \
+                --disable-werror \
+                --disable-symvers \
+                --disable-libstdcxx-pch \
+                --disable-libstdcxx-debug \
+                --disable-isl-version-check"
+            ;;
+    esac
+
+    CFLAGS=$CFLAGS $SOURCE_PATH/$GCC_VERSION/configure \
         --prefix=$TOOLCHAIN_PATH \
         --target=$TARGET \
         --enable-languages=c,lto,c++,fortran \
-        --enable-shared \
         --enable-static \
-        --enable-threads=win32 \
         --enable-graphite \
         --enable-fully-dynamic-string \
         --enable-libstdcxx-filesystem-ts=yes \
@@ -30,30 +54,23 @@ if [ $RUN_CONFIG = 1 ] || [ ! -f "$GCC_BUILD_PATH/Makefile" ] ; then
         --enable-lto \
         --enable-libgomp \
         --enable-checking=release \
-        --disable-multilib \
-        --disable-shared \
-        --disable-rpath \
-        --disable-win32-registry \
-        --disable-werror \
-        --disable-symvers \
-        --disable-libstdcxx-pch \
-        --disable-libstdcxx-debug \
-        --disable-isl-version-check \
         --disable-bootstrap \
+        --disable-multilib \
         --with-libiconv \
         --with-system-zlib \
         --with-gnu-as \
-        --with-gnu-ld
+        --with-gnu-ld \
+        $HOST_OPTION
     echo "::endgroup::"
 fi
 
 echo "::group::Build GCC"
-make $BUILD_MAKE_OPTIONS all-gcc
+    make $BUILD_MAKE_OPTIONS all-gcc
 echo "::endgroup::"
 
 if [ $RUN_INSTALL = 1 ] ; then
     echo "::group::Install GCC"
-    make install-gcc
+        make install-gcc
     echo "::endgroup::"
 fi
 
