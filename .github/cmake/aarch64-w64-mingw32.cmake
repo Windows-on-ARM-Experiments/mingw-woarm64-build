0c0d45d04e809497345e8f44239ca3e218fdfdb3
Radek Bartoň <radek.barton@microsoft.com>
2023-11-28 16:27:01 +0100

    Add CMake toolchain file for using the aarch64-w64-mingw32 toolchain (#8)

---
+++ .github/cmake/aarch64-w64-mingw32.cmake
@@ -0,0 +1,7 @@
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_PROCESSOR ARM64)
+set(CMAKE_C_COMPILER aarch64-w64-mingw32-gcc)
+set(CMAKE_CXX_COMPILER aarch64-w64-mingw32-g++)
+set(CMAKE_RC_COMPILER aarch64-w64-mingw32-windres)
+set(CMAKE_RANLIB aarch64-w64-mingw32-ranlib)
+set(CMAKE_AR aarch64-w64-mingw32-ar)
