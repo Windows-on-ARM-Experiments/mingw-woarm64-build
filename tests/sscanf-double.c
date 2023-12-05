a6b88aa67b2b95856e1133bc3c7a3b2ef2ea3d37
Evgeny Karpov <eukarpov@gmail.com>
2023-12-05 11:43:54 +0100

    * add a test for reading double from string

---
+++ tests/sscanf-double.c
@@ -0,0 +1,16 @@
+/* 
+   Tests reading double from a string
+*/
+
+#include <stdio.h>
+
+int sscanf_double() {
+    double v[3];
+    sscanf("0.0 1.0 0.7", "%lf %lf %lf", v, v + 1, v + 2);
+    printf("%lf %lf %lf\n", v[0], v[1], v[2]);
+ 
+    if (v[0] != 0. || v[1] != 1. || v[2] != 0.7)
+        return 0;
+
+    return 1;
+}
