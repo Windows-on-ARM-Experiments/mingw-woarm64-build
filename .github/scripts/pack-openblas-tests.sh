f1e3a69614327e785c96c5c6b4c18e9ba82594a3
Radek Bartoň <radek.barton@microsoft.com>
2023-12-08 13:13:53 +0100

    Cache toolchain build to speed up testing development

--- .github/scripts/pack-openblas-tests.sh
+++ .github/scripts/pack-openblas-tests.sh
@@ -4,7 +4,7 @@ source `dirname $0`/config.sh
 
 echo "::group::Pack OpenBLAS"
     cd $SOURCE_PATH/OpenBLAS
-    zip aarch64-msvcrt-openblas-tests.zip ctest/x* ctest/cin* ctest/din* ctest/sin* ctest/zin*
+    zip $TOOLCHAIN_NAME-openblas-tests.zip ctest/x* ctest/cin* ctest/din* ctest/sin* ctest/zin*
 echo "::endgroup::"
 
 echo 'Success!'
