00f078bb4beca157163dcf25bcb683aa264f6470
Evgeny Karpov <eukarpov@gmail.com>
2024-01-03 19:18:18 +0100

    * fix tests packing

--- .github/scripts/ffmpeg/pack-ffmpeg.sh
+++ .github/scripts/ffmpeg/pack-ffmpeg.sh
@@ -5,7 +5,7 @@ source `dirname ${BASH_SOURCE[0]}`/../config.sh
 echo "::group::Pack FFmpeg"
     mkdir -p $ARTIFACT_PATH
     cd $FFMPEG_PATH
-    zip -R $ARTIFACT_PATH/$TOOLCHAIN_NAME-ffmpeg.zip fate.log *.exe */*.exe --exclude ffmpeg_g.exe ffprobe_g.exe
+    zip -r $ARTIFACT_PATH/$TOOLCHAIN_NAME-ffmpeg.zip *
 echo "::endgroup::"
 
 echo 'Success!'
