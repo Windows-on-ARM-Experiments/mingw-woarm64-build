82eba0336e9d4a254c1cc5fcf539f6815edd9a6d
Evgeny Karpov <eukarpov@gmail.com>
2024-01-04 23:46:56 +0100

    * refactor params

--- .github/scripts/get-files-sha.sh
+++ .github/scripts/get-files-sha.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 repo_path=$1
-path=${@:2}
 
 git status
 echo $path
-git log -1 --format="%H" $path
+git log -1 --format="%H" ${@:2}
 
 if [ -n "$GITHUB_EVENT_PULL_REQUEST_HEAD_REF" ]; then
 
@@ -19,11 +18,11 @@ if [ -n "$GITHUB_EVENT_PULL_REQUEST_HEAD_REF" ]; then
         git branch $GITHUB_EVENT_PULL_REQUEST_HEAD_REF FETCH_HEAD
     fi
 
-    sha=$(git log -1 --format="%H" origin/$GITHUB_EVENT_PULL_REQUEST_BASE_REF $path)
-    sha=$sha-$(git log -1 --format="%H" origin/$GITHUB_EVENT_PULL_REQUEST_HEAD_REF $path)
+    sha=$(git log -1 --format="%H" origin/$GITHUB_EVENT_PULL_REQUEST_BASE_REF ${@:2})
+    sha=$sha-$(git log -1 --format="%H" origin/$GITHUB_EVENT_PULL_REQUEST_HEAD_REF ${@:2})
     echo $sha
     echo sha=$sha >> "$GITHUB_OUTPUT"
     exit 0
 fi
 
-echo sha=$(cd $repo_path; git log -1 --pretty=format:"%H" $path) >> "$GITHUB_OUTPUT"
+echo sha=$(cd $repo_path; git log -1 --pretty=format:"%H" ${@:2}) >> "$GITHUB_OUTPUT"
