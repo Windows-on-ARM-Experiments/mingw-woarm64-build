94e10b9e57c892fadcf1d839bcbc111db2f62e22
Evgeny Karpov <eukarpov@gmail.com>
2024-01-04 23:53:22 +0100

    * revert debugging information and fetch all history to calculate sha properly

--- .github/scripts/get-files-sha.sh
+++ .github/scripts/get-files-sha.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 repo_path=$1
-
-git status
-echo $path
-git log -1 --format="%H" ${@:2}
+path=${@:2}
 
 if [ -n "$GITHUB_EVENT_PULL_REQUEST_HEAD_REF" ]; then
 
@@ -18,11 +15,11 @@ if [ -n "$GITHUB_EVENT_PULL_REQUEST_HEAD_REF" ]; then
         git branch $GITHUB_EVENT_PULL_REQUEST_HEAD_REF FETCH_HEAD
     fi
 
-    sha=$(git log -1 --format="%H" origin/$GITHUB_EVENT_PULL_REQUEST_BASE_REF ${@:2})
-    sha=$sha-$(git log -1 --format="%H" origin/$GITHUB_EVENT_PULL_REQUEST_HEAD_REF ${@:2})
+    sha=$(git log -1 --format="%H" origin/$GITHUB_EVENT_PULL_REQUEST_BASE_REF $path)
+    sha=$sha-$(git log -1 --format="%H" origin/$GITHUB_EVENT_PULL_REQUEST_HEAD_REF $path)
     echo $sha
     echo sha=$sha >> "$GITHUB_OUTPUT"
     exit 0
 fi
 
-echo sha=$(cd $repo_path; git log -1 --pretty=format:"%H" ${@:2}) >> "$GITHUB_OUTPUT"
+echo sha=$(cd $repo_path; git log -1 --pretty=format:"%H" $path) >> "$GITHUB_OUTPUT"
