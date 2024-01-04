767a677112a689417092b3b3442568eea6bdd932
Evgeny Karpov <eukarpov@gmail.com>
2024-01-04 20:51:57 +0100

    * make sha for pull request

--- .github/scripts/get-files-sha.sh
+++ .github/scripts/get-files-sha.sh
@@ -4,9 +4,19 @@ echo "$GITHUB_EVENT_REPOSITORY_FULL_NAME"
 echo "$GITHUB_EVENT_PULL_REQUEST_BASE_REF"
 echo "$GITHUB_EVENT_PULL_REQUEST_HEAD_REF"
 
-if [ -n "$GITHUB_EVENT_PULL_REQUEST_HEAD_REF" ]; then
+repo_path=$1
+path=$2
 
+get_sha() {
+    sed '/sha/!d;/sha/s/.*: "//;s/",$//;q'
+}
+
+if [ -n "$GITHUB_EVENT_PULL_REQUEST_HEAD_REF" ]; then
+    sha=$(curl -s "https://api.github.com/repos/$GITHUB_EVENT_REPOSITORY_FULL_NAME/commits?sha=$GITHUB_EVENT_PULL_REQUEST_BASE_REF&per_page=1&path=$path" | get_sha)
+    sha=$sha-$(curl -s "https://api.github.com/repos/$GITHUB_EVENT_REPOSITORY_FULL_NAME/commits?sha=$GITHUB_EVENT_PULL_REQUEST_BASE_REF&per_page=1&path=$path" | get_sha)
+    echo $sha
+    echo sha=$sha >> "$GITHUB_OUTPUT"
     exit 0
 fi
 
-echo sha=$(cd $1; git log -1 --pretty=format:"%H" ${@:2}) >> "$GITHUB_OUTPUT"
+echo sha=$(cd $repo_path; git log -1 --pretty=format:"%H" ${@:path}) >> "$GITHUB_OUTPUT"
