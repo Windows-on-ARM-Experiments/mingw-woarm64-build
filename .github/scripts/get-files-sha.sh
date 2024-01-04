139a905b180052f2189d10c163a07a5bea9ef910
Evgeny Karpov <eukarpov@gmail.com>
2024-01-04 19:57:09 +0100

    * use GITHUB_EVENT_REPOSITORY_NAME

--- .github/scripts/get-files-sha.sh
+++ .github/scripts/get-files-sha.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+echo "$GITHUB_EVENT_REPOSITORY_NAME"
 echo "$GITHUB_EVENT_PULL_REQUEST_BASE_REF"
 echo "$GITHUB_EVENT_PULL_REQUEST_HEAD_REF"
+
+if [ -n "$GITHUB_EVENT_PULL_REQUEST_HEAD_REF" ]; then
+
+    exit 0
+fi
+
 echo sha=$(cd $1; git log -1 --pretty=format:"%H" ${@:2}) >> "$GITHUB_OUTPUT"
