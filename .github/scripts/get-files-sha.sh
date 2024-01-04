60240a0d7f52140fe5af7ba0a92ba1e9e9998c24
Evgeny Karpov <eukarpov@gmail.com>
2024-01-04 21:30:43 +0100

    * fetch required branches for sha

--- .github/scripts/get-files-sha.sh
+++ .github/scripts/get-files-sha.sh
@@ -8,6 +8,17 @@ repo_path=$1
 path=${@:2}
 
 if [ -n "$GITHUB_EVENT_PULL_REQUEST_HEAD_REF" ]; then
+
+    if ! git rev-parse --verify "$GITHUB_EVENT_PULL_REQUEST_BASE_REF"; then
+        git fetch origin $GITHUB_EVENT_PULL_REQUEST_BASE_REF
+        git branch $GITHUB_EVENT_PULL_REQUEST_BASE_REF FETCH_HEAD
+    fi
+
+    if ! git rev-parse --verify "$GITHUB_EVENT_PULL_REQUEST_HEAD_REF"; then
+        git fetch origin $GITHUB_EVENT_PULL_REQUEST_HEAD_REF
+        git branch $GITHUB_EVENT_PULL_REQUEST_HEAD_REF FETCH_HEAD
+    fi
+
     sha=$(git log -1 --format="%H" origin/$GITHUB_EVENT_PULL_REQUEST_BASE_REF $path)
     sha=$sha-$(git log -1 --format="%H" origin/$GITHUB_EVENT_PULL_REQUEST_HEAD_REF $path)
     echo $sha
