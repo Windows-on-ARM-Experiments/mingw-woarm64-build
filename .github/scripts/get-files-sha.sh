11f7709b5992d4e6100a023622a12311171480bb
Evgeny Karpov <eukarpov@gmail.com>
2024-01-05 14:57:45 +0100

    * revert script after fixing the workflow

--- .github/scripts/get-files-sha.sh
+++ .github/scripts/get-files-sha.sh
@@ -1,25 +1,3 @@
 #!/bin/bash
 
-repo_path=$1
-path=${@:2}
-
-if [ -n "$HEAD_REF2" ]; then
-
-    if ! git rev-parse --verify "$BASE_REF"; then
-        git fetch origin $BASE_REF
-        git branch $BASE_REF FETCH_HEAD
-    fi
-
-    if ! git rev-parse --verify "$HEAD_REF"; then
-        git fetch origin $HEAD_REF
-        git branch $HEAD_REF FETCH_HEAD
-    fi
-
-    sha=$(git log -1 --format="%H" origin/$BASE_REF $path)
-    sha=$sha-$(git log -1 --format="%H" origin/$HEAD_REF $path)
-    echo $sha
-    echo sha=$sha >> "$GITHUB_OUTPUT"
-    exit 0
-fi
-
-echo sha=$(cd $repo_path; git log -1 --pretty=format:"%H" $path) >> "$GITHUB_OUTPUT"
+echo sha=$(cd $1; git log -1 --pretty=format:"%H" ${@:2}) >> "$GITHUB_OUTPUT"
