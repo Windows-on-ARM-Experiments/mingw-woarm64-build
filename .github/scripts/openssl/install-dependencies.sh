da7d3b05ea47b44955b204a5b115a9f1968a119e
Radek Bartoň <radek.barton@microsoft.com>
2024-01-31 08:38:00 +0100

    Build libxml2 using CMake (#94)

--- .github/scripts/openssl/install-dependencies.sh
+++ .github/scripts/openssl/install-dependencies.sh
@@ -2,7 +2,7 @@
 
 source `dirname ${BASH_SOURCE[0]}`/../config.sh
 
-echo "::group::Install OpenSSL Dependencies"
+echo "::group::Install OpenSSL dependencies"
     sudo apt update
     sudo apt install -y libtext-template-perl
 echo "::endgroup::"
