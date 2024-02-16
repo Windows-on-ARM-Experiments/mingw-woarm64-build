26d7a945d4231cf6111ddb70ed9865f632d6c418
Evgeny Karpov <eukarpov@gmail.com>
2024-02-16 09:45:29 +0100

    Add SscanfDoubleTest

--- tests/main.c
+++ tests/main.c
@@ -10,6 +10,7 @@ TEST(Aarch64MinGW, NestedFunction);
 TEST(Aarch64MinGW, OmpTest);
 TEST(Aarch64MinGW, TestUnwindStack);
 TEST(Aarch64MinGW, SJLJTest);
+TEST(Aarch64MinGW, SscanfDoubleTest);
 TEST(Aarch64MinGW, StructTest);
 TEST(Aarch64MinGW, TestVaList);
 TEST(Aarch64MinGW, TestSPrintf);
@@ -46,6 +47,7 @@ int main(int argc, char **argv) {
         DECLARE_TEST(Aarch64MinGW, OmpTest),
         DECLARE_TEST(Aarch64MinGW, TestUnwindStack),
         DECLARE_TEST(Aarch64MinGW, SJLJTest),
+        DECLARE_TEST(Aarch64MinGW, SscanfDoubleTest),
         DECLARE_TEST(Aarch64MinGW, StructTest),
         DECLARE_TEST(Aarch64MinGW, TestVaList),
         DECLARE_TEST(Aarch64MinGW, TestSPrintf),
