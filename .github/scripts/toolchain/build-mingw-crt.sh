e6ee6a379605afc907f9a846905df4113a532b97
Radek Bartoň <radek.barton@microsoft.com>
2024-03-05 13:37:22 +0100

    Unify how the host and target `configure` options are constructed (#115)

--- .github/scripts/toolchain/build-mingw-crt.sh
+++ .github/scripts/toolchain/build-mingw-crt.sh
@@ -2,7 +2,7 @@
 
 source `dirname ${BASH_SOURCE[0]}`/../config-mingw.sh
 
-MINGW_BUILD_PATH=$BUILD_PATH/mingw
+MINGW_BUILD_PATH=$BUILD_PATH/mingw-crt
 
 mkdir -p $MINGW_BUILD_PATH
 cd $MINGW_BUILD_PATH
@@ -11,22 +11,33 @@ if [ "$RUN_CONFIG" = 1 ] || [ ! -f "$MINGW_BUILD_PATH/Makefile" ] ; then
     echo "::group::Configure MinGW CRT"
         rm -rf $MINGW_BUILD_PATH/*
 
+        if [ "$DEBUG" = 1 ] ; then
+            HOST_OPTIONS="$HOST_OPTIONS \
+                --enable-debug"
+        fi
+
+        case $PLATFORM in
+            *mingw*)
+                TARGET_OPTIONS="$TARGET_OPTIONS \
+                    --with-sysroot=$TOOLCHAIN_PATH/$TARGET"
+                ;;
+        esac
+
         $SOURCE_PATH/$MINGW_VERSION/mingw-w64-crt/configure \
             --prefix=$TOOLCHAIN_PATH/$TARGET \
             --build=$BUILD \
             --host=$TARGET \
-            --with-sysroot=$TOOLCHAIN_PATH \
-            --disable-shared \
-            $MINGW_CONF
+            $HOST_OPTIONS \
+            $TARGET_OPTIONS
     echo "::endgroup::"
 fi
 
-echo "::group::Build MinGW headers"
+echo "::group::Build MinGW CRT"
     make $BUILD_MAKE_OPTIONS
 echo "::endgroup::"
 
 if [ "$RUN_INSTALL" = 1 ] ; then
-    echo "::group::Install MinGW headers"
+    echo "::group::Install MinGW CRT"
         make install
     echo "::endgroup::"
 fi
