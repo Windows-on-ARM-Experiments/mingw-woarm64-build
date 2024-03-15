16e372942d224ef06547cca6131e394f0b5747c9
Radek Bartoň <radek.barton@microsoft.com>
2024-03-15 10:39:33 +0100

    Add workflow for testing GCC regressions (#119)

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -2,10 +2,17 @@
 
 source `dirname ${BASH_SOURCE[0]}`/config-mingw.sh
 
+if [ "$RUN_BOOTSTRAP" = 1 ]; then
+    .github/scripts/install-dependencies.sh
+    .github/scripts/install-libraries.sh
+fi
+
 .github/scripts/binutils/build.sh
+
 if [[ "$PLATFORM" =~ linux ]]; then
     .github/scripts/binutils/install-cross-headers-libs.sh
 fi
+
 if [[ "$PLATFORM" =~ mingw ]]; then
     .github/scripts/toolchain/build-mingw-headers.sh
 fi
