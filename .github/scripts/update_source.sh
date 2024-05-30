ef2289bb9509969ab503a6c7186ed2e58206078d
Evgeny Karpov <eukarpov@gmail.com>
2024-05-30 09:24:23 +0200

    Reuse scripts to build the toolchain with one command. Add toolchain build with single command to CI. Fix params on cleaning ccache

---
+++ .github/scripts/update_source.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source `dirname ${BASH_SOURCE[0]}`/config.sh
+
+echo "::group::Update binutils, gcc and mingw sources"
+    cd "$SOURCE_PATH"
+    git -C "$BINUTILS_VERSION" pull || git clone "$BINUTILS_REPO" -b "$BINUTILS_BRANCH" "$BINUTILS_VERSION" --single-branch --depth 1
+    git -C "$GCC_VERSION" pull || git clone "$GCC_REPO" -b "$GCC_BRANCH" "$GCC_VERSION" --single-branch --depth 1
+    git -C "$MINGW_VERSION" pull || git clone "$MINGW_REPO" -b "$MINGW_BRANCH" "$MINGW_VERSION" --single-branch --depth 1
+echo "::endgroup::"
+
+echo 'Success!'
