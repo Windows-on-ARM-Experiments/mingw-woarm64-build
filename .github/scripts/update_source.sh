672d01fa11521b59bbd8518ad80dd90e64751ae0
Evgeny Karpov <eukarpov@gmail.com>
2024-05-30 09:41:39 +0200

    Create the source folder if it does not exist

--- .github/scripts/update_source.sh
+++ .github/scripts/update_source.sh
@@ -3,6 +3,8 @@
 source `dirname ${BASH_SOURCE[0]}`/config.sh
 
 echo "::group::Update binutils, gcc and mingw sources"
+    [ -d "$SOURCE_PATH" ] && mkdir -p "$SOURCE_PATH"
+
     cd "$SOURCE_PATH"
     git -C "$BINUTILS_VERSION" pull || git clone "$BINUTILS_REPO" -b "$BINUTILS_BRANCH" "$BINUTILS_VERSION" --single-branch --depth 1
     git -C "$GCC_VERSION" pull || git clone "$GCC_REPO" -b "$GCC_BRANCH" "$GCC_VERSION" --single-branch --depth 1
--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
-#! /bin/bash
-
+#!/bin/bash
 # This script downloads and configures packages, 
 # builds and installs the Windows on ARM64 GNU Toolchain.
+
+set -e # exit on error
+set -x # echo on
 
 RUN_BOOTSTRAP=1 UPDATE_SOURCE=1 .github/scripts/build.sh
 
