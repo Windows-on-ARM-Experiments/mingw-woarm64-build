8555e04cae1f86dae0589ec85733102027f1ff79
Evgeny Karpov <eukarpov@gmail.com>
2024-05-30 10:38:27 +0200

    Configure the workflow's environment

--- build.sh
+++ build.sh
@@ -5,15 +5,18 @@
 set -e # exit on error
 set -x # echo on
 
-BINUTILS_REPO=${BINUTILS_REPO:-https://github.com/Windows-on-ARM-Experiments/binutils-woarm64.git}
-BINUTILS_BRANCH=${BINUTILS_BRANCH:-woarm64}
+export BINUTILS_REPO=${BINUTILS_REPO:-https://github.com/Windows-on-ARM-Experiments/binutils-woarm64.git}
+export BINUTILS_BRANCH=${BINUTILS_BRANCH:-woarm64}
 
-GCC_REPO=${GCC_REPO:-https://github.com/Windows-on-ARM-Experiments/gcc-woarm64.git}
-GCC_BRANCH=${GCC_BRANCH:-woarm64}
+export GCC_REPO=${GCC_REPO:-https://github.com/Windows-on-ARM-Experiments/gcc-woarm64.git}
+export GCC_BRANCH=${GCC_BRANCH:-woarm64}
 
-MINGW_REPO=${MINGW_REPO:-https://github.com/Windows-on-ARM-Experiments/mingw-woarm64.git}
-MINGW_BRANCH=${MINGW_BRANCH:-woarm64}
+export MINGW_REPO=${MINGW_REPO:-https://github.com/Windows-on-ARM-Experiments/mingw-woarm64.git}
+export MINGW_BRANCH=${MINGW_BRANCH:-woarm64}
 
-RUN_BOOTSTRAP=1 UPDATE_SOURCE=1 .github/scripts/build.sh
+export RUN_BOOTSTRAP=${RUN_BOOTSTRAP:-1}
+export UPDATE_SOURCE=${UPDATE_SOURCE:-1}
+
+.github/scripts/build.sh
 
 echo 'Success!'
