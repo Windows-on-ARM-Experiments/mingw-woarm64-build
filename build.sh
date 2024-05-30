809f7b3f1df66d0638315ad1e47571bdfc058370
Evgeny Karpov <eukarpov@gmail.com>
2024-05-30 09:34:42 +0200

    Add bootstrapping to the script

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 # This script downloads and configures packages, 
 # builds and installs the Windows on ARM64 GNU Toolchain.
 
-UPDATE_SOURCE=1 .github/scripts/build.sh
+RUN_BOOTSTRAP=1 UPDATE_SOURCE=1 .github/scripts/build.sh
 
 echo 'Success!'
