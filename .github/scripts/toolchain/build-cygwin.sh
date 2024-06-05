3eb38fcd92873760e4a8c2f8d99334b83bf5e538
Evgeny Karpov <eukarpov@gmail.com>
2024-06-05 19:31:31 +0200

    Revert "Use only C language for cygwin build"
    This reverts commit 340efeb19c5ad5c2ea3d9410ebb3d75b15007197.

--- .github/scripts/toolchain/build-cygwin.sh
+++ .github/scripts/toolchain/build-cygwin.sh
@@ -32,7 +32,6 @@ if [[ "$RUN_CONFIG" = 1 ]] || [[ ! -f "$CYGWIN_BUILD_PATH/Makefile" ]]; then
             --build=$HOST \
             --host=$HOST \
             --target=$TARGET \
-            --enable-languages=c \
             --enable-static \
             --disable-dumper \
             --with-sysroot=$TOOLCHAIN_PATH \
