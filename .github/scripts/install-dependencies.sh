9cd0d720df8a16fc76cd8e0c3d49ef3d1d677a5c
Radek Bartoň <radek.barton@microsoft.com>
2024-07-01 20:32:11 +0200

    Shared `libgcc` can now be enabled for `aarch64-w64-mingw32`

--- .github/scripts/install-dependencies.sh
+++ .github/scripts/install-dependencies.sh
@@ -5,6 +5,8 @@ source `dirname ${BASH_SOURCE[0]}`/config.sh
 echo "::group::Install Dependencies"
     sudo apt update
     sudo apt install -y build-essential autoconf automake autotools-dev binutils-for-build texinfo bison flex ccache docbook2x xmlto zlib1g-dev libc6-dev-arm64-cross libc6-dev-amd64-cross dejagnu
+
+    which aclocal
 echo "::endgroup::"
 
 echo 'Success!'
