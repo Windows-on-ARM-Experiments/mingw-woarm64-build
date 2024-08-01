bffabf137335980a73eac6240da02b852632fdea
Radek Bartoň <blackhex@post.cz>
2024-08-01 21:23:24 +0200

    Add option to select single GCC module, tests filter, and target and host boards to `execute-gcc-tests.sh`

--- .github/scripts/toolchain/execute-gcc-tests.sh
+++ .github/scripts/toolchain/execute-gcc-tests.sh
@@ -5,6 +5,11 @@ CCACHE=0 # Disable ccache for testing.
 source `dirname ${BASH_SOURCE[0]}`/../config.sh
 
 TAG=$1
+MODULE=$2
+FILTER=$3
+TARGET_BOARD=${4:-wsl-sim}
+HOST_BOARD=${5:-}
+
 GCC_BUILD_PATH=$BUILD_PATH/gcc
 TEST_RESULTS_PATH=$ARTIFACT_PATH/gcc-tests-$TAG
 
@@ -14,8 +19,19 @@ echo "::group::Execute GCC tests"
     done
 
     cd $GCC_BUILD_PATH
-    make $BUILD_MAKE_OPTIONS -k check \
-        RUNTESTFLAGS="--target_board wsl-sim" \
+    if [[ -z "$MODULE" ]]; then
+        MAKE_TARGET="check"
+    else
+        MAKE_TARGET="check-$MODULE"
+    fi
+    if [[ -n "$TARGET_BOARD" ]]; then
+        TARGET_BOARD="--target-board $TARGET_BOARD"
+    fi
+    if [[ -n "$HOST_BOARD" ]]; then
+        HOST_BOARD="--host-board $HOST_BOARD"
+    fi
+    make $BUILD_MAKE_OPTIONS -k $MAKE_TARGET \
+        RUNTESTFLAGS="$FILTER $HOST_BOARD $TARGET_BOARD" \
         DEJAGNU="$DEJAGNU_FILE" \
         CHECK_TEST_FRAMEWORK=1 \
         || echo "Error"
