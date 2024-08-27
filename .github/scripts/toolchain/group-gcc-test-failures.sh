4bb296eb13817bf1db60d66f9bc1a3b9512ee5ee
Martin Vejbora <mvejbora@microsoft.com>
2024-08-27 15:53:06 +0200

    Don't store bash parent dir for consistency with other scripts

--- .github/scripts/toolchain/group-gcc-test-failures.sh
+++ .github/scripts/toolchain/group-gcc-test-failures.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-SCRIPT_DIRNAME=$(dirname ${BASH_SOURCE[0]})
-
-source $SCRIPT_DIRNAME/../config.sh
+source `dirname ${BASH_SOURCE[0]}`/../config.sh
 
 TAG=$1
 GITHUB_STEP_SUMMARY=$2
 
-python3 $SCRIPT_DIRNAME/group-gcc-test-failures.py \
+python3 `dirname ${BASH_SOURCE[0]}`/group-gcc-test-failures.py \
     --dir $ARTIFACT_PATH/gcc-tests-$TAG \
     --summary_output $GITHUB_STEP_SUMMARY >> \
     $ARTIFACT_PATH/gcc-tests-$TAG/grouped-test-failures.txt
