6981583ef01418ae8223685076f1d285f461ed64
Martin Vejbora <mvejbora@microsoft.com>
2024-08-28 15:33:08 +0200

    Adapt extract-most-frequent-failures.sh to parse markdown input

--- .github/scripts/toolchain/extract-most-frequent-failures.sh
+++ .github/scripts/toolchain/extract-most-frequent-failures.sh
@@ -2,13 +2,14 @@
 
 echo "## 10 directories with most test failures:"
 
-sed -e '1,/^Directory structure of failed tests with failure count:/d' |
-grep -v "\.\.\." |
+sed -e '1,/^## Directory structure of failed tests with failure count:/d' |
+sed "s/<[^>]*>//g" |
+sed 's/-  */- /g' |
+grep -v "^[[:space:]]*$" |
 awk -F ': ' '{print $2, NR, $0}' |
 sort -nr |
 head -n 10 |
 cut -f2- -d' ' |
 # sort by line number, so that nested directories are correctly grouped
 sort -n |
-cut -f2- -d' ' |
-sed -E "s/^[[:space:]]*/&- /"
+cut -f2- -d' '
