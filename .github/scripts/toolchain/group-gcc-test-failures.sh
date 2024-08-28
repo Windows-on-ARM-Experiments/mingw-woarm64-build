c1d76103cb3937c831c22a40eb9fdadcd4174de1
Martin Vejbora <mvejbora@microsoft.com>
2024-08-28 16:29:58 +0200

    Format multiline commands

--- .github/scripts/toolchain/group-gcc-test-failures.sh
+++ .github/scripts/toolchain/group-gcc-test-failures.sh
@@ -4,9 +4,11 @@ source `dirname ${BASH_SOURCE[0]}`/../config.sh
 
 TAG=$1
 
-python3 `dirname ${BASH_SOURCE[0]}`/group-gcc-test-failures.py \
+BASH_SOURCE_DIR=`dirname ${BASH_SOURCE[0]}`
+
+python3 $BASH_SOURCE_DIR/group-gcc-test-failures.py \
     --dir $ARTIFACT_PATH/gcc-tests-$TAG >> \
     $ARTIFACT_PATH/gcc-tests-$TAG/grouped-test-failures.md
 
 cat $ARTIFACT_PATH/gcc-tests-$TAG/grouped-test-failures.md |
-`dirname ${BASH_SOURCE[0]}`/extract-most-frequent-failures.sh >> $GITHUB_STEP_SUMMARY
+    $BASH_SOURCE_DIR/extract-most-frequent-failures.sh >> $GITHUB_STEP_SUMMARY
