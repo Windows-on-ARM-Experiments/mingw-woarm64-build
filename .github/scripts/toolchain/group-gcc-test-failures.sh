2756d5aac22c7daf68245c767abe6a8fcdc0b167
Martin Vejbora <mvejbora@microsoft.com>
2024-08-28 15:34:25 +0200

    Use markdown extension for grouped failures output

--- .github/scripts/toolchain/group-gcc-test-failures.sh
+++ .github/scripts/toolchain/group-gcc-test-failures.sh
@@ -6,7 +6,7 @@ TAG=$1
 
 python3 `dirname ${BASH_SOURCE[0]}`/group-gcc-test-failures.py \
     --dir $ARTIFACT_PATH/gcc-tests-$TAG >> \
-    $ARTIFACT_PATH/gcc-tests-$TAG/grouped-test-failures.txt
+    $ARTIFACT_PATH/gcc-tests-$TAG/grouped-test-failures.md
 
-cat $ARTIFACT_PATH/gcc-tests-$TAG/grouped-test-failures.txt |
+cat $ARTIFACT_PATH/gcc-tests-$TAG/grouped-test-failures.md |
 `dirname ${BASH_SOURCE[0]}`/extract-most-frequent-failures.sh >> $GITHUB_STEP_SUMMARY
