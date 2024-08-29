b634287f03b81ddbb6a3d0bfd84cdd9ba350d0c7
Martin Vejbora <mvejbora@microsoft.com>
2024-08-29 14:00:03 +0200

    Add ROOT_DIR var into config script

--- .github/scripts/toolchain/group-gcc-test-failures.sh
+++ .github/scripts/toolchain/group-gcc-test-failures.sh
@@ -5,11 +5,9 @@ source `dirname ${BASH_SOURCE[0]}`/../config.sh
 TAG=$1
 SUMMARY_OUTPUT=$2
 
-BASH_SOURCE_DIR=`dirname ${BASH_SOURCE[0]}`
-
-python3 $BASH_SOURCE_DIR/group-gcc-test-failures.py \
+python3 $ROOT_DIR/.github/scripts/toolchain/group-gcc-test-failures.py \
     --dir $ARTIFACT_PATH/gcc-tests-$TAG >> \
     $ARTIFACT_PATH/gcc-tests-$TAG/grouped-test-failures.md
 
 cat $ARTIFACT_PATH/gcc-tests-$TAG/grouped-test-failures.md |
-    $BASH_SOURCE_DIR/extract-most-frequent-failures.sh 10 >> $SUMMARY_OUTPUT
+    $ROOT_DIR/.github/scripts/toolchain/extract-most-frequent-failures.sh 10 >> $SUMMARY_OUTPUT
