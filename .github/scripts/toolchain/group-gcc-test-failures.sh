763c14fa6599efffb359f30d5d15b4d1df997a24
Radek Bartoň <radek.barton@microsoft.com>
2024-09-02 12:00:12 +0200

    Use absolute paths to scripts in `build-and-test-toolchain.yml`

--- .github/scripts/toolchain/group-gcc-test-failures.sh
+++ .github/scripts/toolchain/group-gcc-test-failures.sh
@@ -5,9 +5,9 @@ source `dirname ${BASH_SOURCE[0]}`/../config.sh
 TAG=$1
 SUMMARY_OUTPUT=$2
 
-python3 $ROOT_DIR/.github/scripts/toolchain/group-gcc-test-failures.py \
+python3 $ROOT_PATH/.github/scripts/toolchain/group-gcc-test-failures.py \
     --dir $ARTIFACT_PATH/gcc-tests-$TAG >> \
     $ARTIFACT_PATH/gcc-tests-$TAG/grouped-test-failures.md
 
 cat $ARTIFACT_PATH/gcc-tests-$TAG/grouped-test-failures.md |
-    $ROOT_DIR/.github/scripts/toolchain/extract-most-frequent-failures.sh 10 >> $SUMMARY_OUTPUT
+    $ROOT_PATH/.github/scripts/toolchain/extract-most-frequent-failures.sh 10 >> $SUMMARY_OUTPUT
