092b46cca57dd453c8e4192cd0e947ebeef2ab42
Radek Bartoň <radek.barton@microsoft.com>
2024-10-21 17:43:51 +0200

    Add script for finding build regression using `git bisect`

---
+++ tools/find-regression-run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+source `dirname ${BASH_SOURCE[0]}`/../.github/scripts/config.sh
+
+BRANCH=$1
+
+COMMIT=$(git rev-parse HEAD)
+git cherry-pick $(git merge-base $BRANCH HEAD)..$BRANCH
+
+set +e
+    RUN_BOOTSTRAP=0 \
+    UPDATE_SOURCES=0 \
+    RESET_SOURCES=1 \
+    APPLY_PATCHES=1 \
+      $ROOT_PATH/.github/scripts/build.sh
+    RESULT=$?
+set -e
+
+git reset --hard $COMMIT
+
+if [[ $RESULT -eq 0 ]]; then
+    echo 'Success!'
+else
+    echo 'Failure!'
+fi
+
+exit $RESULT
