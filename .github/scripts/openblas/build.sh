e48fb3cf2bf2046c7ea8222e2ba5053c507aba79
Radek Bartoň <radek.barton@microsoft.com>
2024-12-01 21:42:07 +0100

    Set supported C standard explicitly for OpenBLAS build

--- .github/scripts/openblas/build.sh
+++ .github/scripts/openblas/build.sh
@@ -11,7 +11,7 @@ fi
 
 echo "::group::Build OpenBLAS"
     cd $SOURCE_PATH/openblas
-    make BINARY=64 CC=aarch64-w64-mingw32-gcc CFLAGS=-Wno-incompatible-pointer-types HOSTCC=gcc NOFORTRAN=1 TARGET=ARMV8 $BUILD_MAKE_OPTIONS
+    make BINARY=64 CC=aarch64-w64-mingw32-gcc CFLAGS="-std=gnu17" HOSTCC=gcc NOFORTRAN=1 TARGET=ARMV8 $BUILD_MAKE_OPTIONS
     for i in ctest/x*; do 
         mv $i $i.exe
     done
