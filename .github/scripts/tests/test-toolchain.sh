64f1f7e5ca2685421dc8bf565f43bef0f881cd67
Martin Vejbora <mvejbora@microsoft.com>
2024-12-10 10:36:37 +0100

    Use Dejagnu test from binutils for testing big-obj

--- .github/scripts/tests/test-toolchain.sh
+++ .github/scripts/tests/test-toolchain.sh
@@ -1,36 +1,27 @@
 #!/bin/bash
 
-source `dirname ${BASH_SOURCE[0]}`/../config.sh
+CCACHE=0 # Disable ccache for testing.
 
-AS="$TOOLCHAIN_PATH/bin/$TARGET-as"
-OBJDUMP="$TOOLCHAIN_PATH/bin/$TARGET-objdump"
-ASM_SOURCE="$SOURCE_PATH/binutils/gas/testsuite/gas/pe/big-obj.s"
+source `dirname ${BASH_SOURCE[0]}`/../config.sh
 
 echo "::group::Test GAS big object PE/COFF format"
 
-    cd $ROOT_PATH/tests
+    ls -l $ROOT_PATH
+    ls -l $BUILD_PATH
+    cd $BUILD_PATH/binutils
 
     EXIT_CODE=0
-    $AS $ASM_SOURCE -o big-obj.o || EXIT_CODE=$?
+    make $BUILD_MAKE_OPTIONS check-gas \
+        RUNTESTFLAGS="pe.exp=big-obj --target_board=wsl-sim" \
+        DEJAGNU="$DEJAGNU_FILE" \
+        CHECK_TEST_FRAMEWORK=1 \
+        || EXIT_CODE=$?
 
-    if [ $EXIT_CODE -eq 0 ]; then
-        echo "Unexpected success, aarch64-w64-mingw32-as should default to small object PE/COFF format"
-        exit 1
-    fi
-
-    EXIT_CODE=0
-    $AS -mbig-obj $ASM_SOURCE -o big-obj.o || EXIT_CODE=$?
     if [ $EXIT_CODE -ne 0 ]; then
-        echo "Failure, aarch64-w64-mingw32-as with -mbig-obj argument failed to compile"
-        exit 1
-    fi
-
-    # Search for the .data$a49999 section in the object file
-    EXIT_CODE=0
-    $OBJDUMP -h  big-obj.o | grep -Pzo '5000. \.data\$a49999  .*\n *CONTENTS, ALLOC, LOAD, DATA' || EXIT_CODE=$?
-    if [ $EXIT_CODE -ne 0 ]; then
-        echo "Failure, aarch64-w64-mingw32-as with -mbig-obj argument could not create object file with a big number of sections"
+        echo "Failure! aarch64-w64-mingw32-as with -mbig-obj argument could not create object file with a big number of sections"
         exit 1
     fi
 
 echo "::endgroup::"
+
+echo 'Success!'
