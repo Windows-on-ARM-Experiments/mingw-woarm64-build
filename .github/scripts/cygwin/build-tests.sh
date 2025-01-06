4c49e1205142ab094016880917cfd019b15f9bf6
Evgeny Karpov <eukarpov@gmail.com>
2025-01-06 18:57:53 +0100

    Add cygwin toolchain tests to the workflow

---
+++ .github/scripts/cygwin/build-tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source `dirname ${BASH_SOURCE[0]}`/../config.sh
+
+PATH="$TOOLCHAIN_PATH/bin:$TOOLCHAIN_PATH/lib/gcc/aarch64-pc-cygwin/:$TOOLCHAIN_PATH/lib/gcc/$TARGET/$GCC_VERSION/"
+
+echo "::group::Build Cygwin AArch64 tests"
+    pushd $ROOT_PATH/tests/cygwin
+        mkdir -p $TESTS_PATH
+        aarch64-pc-cygwin-gcc -o $TESTS_PATH/hello-cygwin.exe hello-cygwin.c
+        cp $TOOLCHAIN_PATH/bin/cygwin1.dll $TESTS_PATH
+    popd
+echo "::endgroup::"
