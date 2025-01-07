bf6b0fa15918571b69f088046d249d5c9f20770e
Evgeny Karpov <eukarpov@gmail.com>
2025-01-08 00:32:32 +0100

    Fix extract cache keys script

--- .github/scripts/extract-cache-keys.sh
+++ .github/scripts/extract-cache-keys.sh
@@ -4,5 +4,5 @@ source `dirname ${BASH_SOURCE[0]}`/config.sh
 
 job_keys_json=$1
 
-toolchain_cache_key=$(echo $job_keys_json | jq -r '.["${{ env.TOOLCHAIN_NAME }}-toolchain-cache-key"]')
+toolchain_cache_key=$(echo $job_keys_json | jq -r '.["$TOOLCHAIN_NAME-toolchain-cache-key"]')
 echo "toolchain-cache-key=$toolchain_cache_key" >> "$GITHUB_ENV"
