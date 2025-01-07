e247a9e897cb02595c3f571d1c1a47c3a922c822
Evgeny Karpov <eukarpov@gmail.com>
2025-01-07 21:22:24 +0100

    Extract toolchain cache key

---
+++ .github/scripts/extract-cache-key.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source `dirname ${BASH_SOURCE[0]}`/config.sh
+
+job_keys_json=$1
+
+toolchain_cache_key=$(echo $job_keys_json | jq -r '.["${{ env.TOOLCHAIN_NAME }}-toolchain-cache-key"]')
+echo "toolchain-cache-key=$toolchain_cache_key" >> "$GITHUB_ENV"
