cd0504d0680944d6d43b60b152d474f795e7ed39
Evgeny Karpov <eukarpov@gmail.com>
2025-01-08 16:26:05 +0100

    Fix cygwin test download and execution

--- .github/scripts/cygwin/execute-tests.sh
+++ .github/scripts/cygwin/execute-tests.sh
@@ -2,13 +2,10 @@
 
 source `dirname ${BASH_SOURCE[0]}`/../config.sh
 
-PATH="$TOOLCHAIN_PATH/bin:$TOOLCHAIN_PATH/lib/gcc/aarch64-pc-cygwin/:$TOOLCHAIN_PATH/lib/gcc/$TARGET/$GCC_VERSION/"
-
 echo "::group::Execute Cygwin AArch64 tests"
-    pushd $ROOT_PATH/tests/cygwin
-        aarch64-pc-cygwin-gcc -o test.exe hello.c
+    pushd $ARTIFACT_PATH
         set +e
-        message=$(./test.exe)
+        message=$(./hello-cygwin.exe)
         error_code=$?
         set -e
         if [[ "$message" == "Hello Cygwin!" && $error_code == 11 ]]; then
