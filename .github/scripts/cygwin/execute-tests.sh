fdc3e6561bfbbcd59e496b802f918cd4eeb64c3b
Evgeny Karpov <eukarpov@gmail.com>
2025-01-09 19:25:14 +0100

    Validate "Hello Cygwin" output

--- .github/scripts/cygwin/execute-tests.sh
+++ .github/scripts/cygwin/execute-tests.sh
@@ -5,11 +5,10 @@ source `dirname ${BASH_SOURCE[0]}`/../config.sh
 echo "::group::Execute Cygwin AArch64 tests"
     pushd $ARTIFACT_PATH
         set +e
-        # Current workarounds are not enough to catch output stream however it might be validated manually
-        powershell -Command ".\hello-cygwin.exe"
         message=$(powershell -Command ".\hello-cygwin.exe; echo \$LASTEXITCODE")
         set -e
-        if [[ "$message" == "11" ]]; then
+        message=$(echo $message | tr '\n' ' ')
+        if [[ "$message" == "Hello Cygwin! 11 " ]]; then
             echo "Test passed"
         else
             echo "Test failed"
