c23cc89442df9a22587661b09b04e7bfa2313643
Evgeny Karpov <eukarpov@gmail.com>
2025-01-21 16:19:16 +0100

    Refactor based on reviews

--- .github/scripts/cygwin/build-tests.sh
+++ .github/scripts/cygwin/build-tests.sh
@@ -2,12 +2,12 @@
 
 source `dirname ${BASH_SOURCE[0]}`/../config.sh
 
-PATH="$TOOLCHAIN_PATH/bin:$TOOLCHAIN_PATH/lib/gcc/aarch64-pc-cygwin/:$TOOLCHAIN_PATH/lib/gcc/$TARGET/$GCC_VERSION/:$PATH"
+PATH="$TOOLCHAIN_PATH/bin:$TOOLCHAIN_PATH/lib/gcc/$TARGET/:$TOOLCHAIN_PATH/lib/gcc/$TARGET/$GCC_VERSION/:$PATH"
 
 echo "::group::Build Cygwin AArch64 tests"
     pushd $ROOT_PATH/tests/cygwin
         mkdir -p $TESTS_PATH
-        aarch64-pc-cygwin-gcc -o $TESTS_PATH/hello-cygwin.exe hello-cygwin.c
+        $TARGET-gcc -o $TESTS_PATH/hello-cygwin.exe hello-cygwin.c
         cp $TOOLCHAIN_PATH/bin/cygwin1.dll $TESTS_PATH
     popd
 echo "::endgroup::"
