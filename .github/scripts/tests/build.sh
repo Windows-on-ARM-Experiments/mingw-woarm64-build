be0c49d8a924313e4ea8ae7b29987c2683b3e627
Evgeny Karpov <eukarpov@gmail.com>
2025-05-16 12:28:35 +0200

    Revert default branch Refactor based on the review

--- .github/scripts/tests/build.sh
+++ .github/scripts/tests/build.sh
@@ -2,15 +2,15 @@
 
 source `dirname ${BASH_SOURCE[0]}`/../config.sh
 
+cd $ROOT_PATH/tests
+
+if [[ "$RUN_CONFIG" = 1 ]] || [[ ! -f "build/Makefile" ]]; then
+  echo "::group::Configure GCC"
+    rm -rf build
+    cmake -S . -B build
+  echo "::endgroup::"
+fi
+
 echo "::group::Build Aarch64 tests"
-    cd $ROOT_PATH/tests
-
-    if [[ "$RUN_CONFIG" = 1 ]] || [[ ! -f "build/Makefile" ]]; then
-      echo "::group::Configure GCC"
-        rm -rf build
-        cmake -S . -B build
-      echo "::endgroup::"
-    fi
-
-    cmake --build build
+  cmake --build build
 echo "::endgroup::"
